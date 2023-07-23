7c011b5ceeea6bd497f2780242547b0439a1c5da
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-23 22:21:57 +0530

    Slicing in python

---
+++ slicing.py
@@ -0,0 +1,21 @@
+# Slicing
+a=['1','2','3','4']
+b=a[0:2]
+print(b)
+# ['1', '2']
+
+b=a[2:]
+print(b)
+# ['3', '4']
+
+b=a[0:-1]
+print(b)
+# ['1', '2', '3']
+
+print(len(b))
+# 3
+
+
+s="Hello world"
+print(s[0:-1])
+# Hello worl
