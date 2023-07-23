073b03c1d96cdadeb41abfa05ae1be85727c7fd7
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-23 13:02:24 +0530

    python data types

---
+++ demo.py
@@ -0,0 +1,31 @@
+# Data Types of Python
+# integer
+print(123)
+# decimal
+print(3.76)
+# String
+print("kalana")
+# Boolean
+print(True)
+print(False)
+# Complex number
+print(2+4j)
+
+# Escape charactor...\n...(new line)
+print("hello")
+print("world")
+
+print("hello...\nworld")
+
+# Escape charactor...\t...(tab space)
+print("name\t: Kalana")
+print("country\t: SriLanka")
+
+# Escape charactor..skip the double quotation ""....\ only
+print("saman said \"Good Morning\" ")
+print("saman said \'Good Morning\' ")
+
+#  enter \ for print specific charactors
+print("special chaactor \\n is give a new line") 
+
+
