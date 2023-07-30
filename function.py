9badaddc47b594da91fcdef1354c364f4cfc1124
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-30 09:14:09 +0530

    default arguments in functions

--- function.py
+++ function.py
@@ -26,3 +26,24 @@ get_results(89)
 # B
 # invalid
 # A
+
+#default arguments
+def get_results(marks, subject="unknown"):
+    print("My subject is ",subject)
+    if marks<0 or marks>100:
+        print("invalid")
+    elif marks<35:
+        print("S")
+    elif marks<50:
+        print("C")
+    elif marks<75:
+        print("B")
+    else:
+        print("A")
+
+get_results(56)
+get_results(67,"Maths")
+# My subject is  unknown
+# B
+# My subject is  Maths
+# B
